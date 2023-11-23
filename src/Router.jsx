243bf0e61c9c800c7be6c809c4fb1fc14bbb3e37
Cheryl Tan <cheryltan134@gmail.com>
2023-11-23 21:29:26 +0800

    update homepage routing

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homepage from './homepage';
+import Homepage from './Homepage';
 import SequenceInput from './SequenceInput';
 import MySequences from './MySequences';
 import SaveSequence from './SaveSequence';
